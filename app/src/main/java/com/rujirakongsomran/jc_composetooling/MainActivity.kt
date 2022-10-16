2a8ba0d939291e73861ddd09cd3c2aa8a9aa4a13
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-10-16 10:22:08 +0700

    Create SurveyAnswer func

--- app/src/main/java/com/rujirakongsomran/jc_composetooling/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_composetooling/MainActivity.kt
@@ -3,12 +3,21 @@ package com.rujirakongsomran.jc_composetooling
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RadioButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.rujirakongsomran.jc_composetooling.ui.theme.JC_ComposeToolingTheme
 
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,14 +39,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun SurveyAnswer() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .wrapContentWidth(Alignment.Start)
+        )
+        Text(
+            text = "Cat",
+            color = Color(0xFF2E7D32)
+        )
+        RadioButton(
+            selected = false,
+            onClick = { /*TODO*/ }
+        )
+    }
 
-@Preview(showBackground = true)
+}
+@Preview
 @Composable
-fun DefaultPreview() {
+fun SurveyAnswerPreview() {
     JC_ComposeToolingTheme {
-        Greeting("Android")
+        SurveyAnswer()
     }
 }
